9c0c2c60736b3a530cb465b5203cb61357edba52
Muhamad Yasir <iryas.muhamad@unkhair.ac.id>
2025-01-23 15:53:14 +0900

    redirect auto https jika mode production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (App::environment(['staging', 'production'])) {
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
