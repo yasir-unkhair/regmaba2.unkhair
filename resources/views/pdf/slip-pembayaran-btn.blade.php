e01433cbda433854558833b0b7d0e31a798e4fec
Muhamad Yasir <muhamadyasir@tik-unkhair.local>
2025-03-14 11:31:21 +0900

    hiangkan logo cetak slik pembayaran

--- resources/views/pdf/slip-pembayaran-btn.blade.php
+++ resources/views/pdf/slip-pembayaran-btn.blade.php
@@ -39,15 +39,19 @@
 <body>
     <table>
         <tr>
-            <td {{-- style='width:20%;border-bottom:0px;border-right:0px;border-top:0px;border-left:0px;vertical-align: middle;text-align: center;text-align:center'>
-                    <img src='{{ get_image(public_path('images/logo.jpg')) }}' height=50 width=50 /> --}} </td>
+            <td
+                style='width:20%;border-bottom:0px;border-right:0px;border-top:0px;border-left:0px;vertical-align: middle;text-align: center;text-align:center'>
+                {{-- <img src='{{ get_image(public_path('images/logo.jpg')) }}' height=50 width=50 /> --}}
+            </td>
             <td
                 style='width:60%;border-bottom:0px;border-right:0px;border-top:0px;border-left:0px;vertical-align: middle;text-align: center;text-align:center'>
                 <h3>Billing Tagihan Pembayaran<br />
                     Universitas Khairun {{ date('Y') }}</h3>
             </td>
-            <td {{-- style='width:20%;border-bottom:0px;border-right:0px;border-top:0px;border-left:0px;vertical-align: middle;text-align: center;text-align:center'>
-                    <img src='{{ get_image(public_path('images/btn.jpeg')) }}' height=50 width=50 /> --}} </td>
+            <td
+                style='width:20%;border-bottom:0px;border-right:0px;border-top:0px;border-left:0px;vertical-align: middle;text-align: center;text-align:center'>
+                {{-- <img src='{{ get_image(public_path('images/btn.jpeg')) }}' height=50 width=50 /> --}}
+            </td>
         </tr>
     </table>
 
