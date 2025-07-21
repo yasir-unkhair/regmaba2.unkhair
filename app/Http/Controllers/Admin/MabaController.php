4f79c688ba307fd7788d250aa277f2b3b83600d4
Muhamad Yasir <muhamadyasir@tik-unkhair.local>
2025-07-21 20:32:49 +0900

    perbaikan bug generate npm

--- app/Http/Controllers/Admin/MabaController.php
+++ app/Http/Controllers/Admin/MabaController.php
@@ -171,9 +171,7 @@ class MabaController extends Controller
             // set pelunasan ukt
             $pembyaran = PesertauktPembayaran::with('peserta')->where('peserta_id', $peserta_id)->where('jenis_pembayaran', 'ukt')->first();
 
-            dd($pembyaran);
-
-            if (!$pembyaran->lunas) {
+            if ($pembyaran && !$pembyaran->lunas) {
                 $pembyaran->update([
                     'lunas' => 1,
                     'tgl_pelunasan' => now()
